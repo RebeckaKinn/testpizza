4e50de2c192d9b9ac7a94b519d889921ca9e91f1
RebeckaKinn <rebeckaki@getacademy.no>
2022-10-20 14:06:00 +0200

    la til js

---
+++ function.js
@@ -0,0 +1,30 @@
+function open1() {
+    document.getElementById("img3").innerHTML=``;
+    document.getElementById("img2").innerHTML=``;
+    document.getElementById("img1").innerHTML=/*htmn*/`
+    <div class="img2" id="img1">
+    <img src="img/pizza1.webp" width="200" height="150"/>
+    </div>
+`; 
+}; 
+
+function open2() {
+    document.getElementById("img1").innerHTML=``;
+    document.getElementById("img3").innerHTML=``;
+    document.getElementById("img2").innerHTML=/*htmn*/`
+    <div class="img2" id="img2">
+    <img src="img/pizza1.webp" width="150" height="100"/>
+    </div>
+`;
+}
+
+function open3() {
+    document.getElementById("img1").innerHTML=``;
+    document.getElementById("img2").innerHTML=``;
+    document.getElementById("img3").innerHTML=/*htmn*/`
+    <div class="img2" id="img3">
+    <img src="img/pizza1.webp" width="100" height="50"/>
+    </div>
+
+    `;
+}
